5e46d6d6bfa5ef52575bbfb772eee2623e59bc5c
heytitle <pat.chormai@gmail.com>
2016-03-12 15:15:37 +0100

    Be able to add tz now and using local storage

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-var app = angular.module('mainApp', [])
+var app = angular.module('mainApp', ['LocalStorageModule'])
 
-app.controller('MainController', function($scope) {
-    $scope.list = [
-        'Asia/Bangkok',
-        'Europe/Amsterdam'
-    ];
+app.controller('MainController', function($scope, localStorageService ) {
 
+    var list = localStorageService.get('tz-list');
+    if( !list || list.length == 0 ){
+        list = [
+            'Asia/Bangkok',
+            'Europe/Amsterdam'
+        ];
+    }
+    console.log(list );
+    $scope.list     = list;
     $scope.moment   = moment;
     $scope.parseInt = window.parseInt;
+
+    $scope.tz = moment.tz.names();
+
+    $scope.addTimeZone = function(){
+        var tz = $scope.tz_selector;
+        $scope.list.push(tz);
+        $scope.tz_selector = "none";
+        localStorageService.set('tz-list', $scope.list );
+    };
+
+    $scope.remove = function(index){
+        $scope.list.splice(index, 1);
+        localStorageService.set('tz-list', $scope.list );
+    };
 
 });
 
